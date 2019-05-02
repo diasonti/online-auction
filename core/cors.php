de34de09164cd5e75b8399052befbd7a83fdef16
Vladimir Danilov <diasonti@gmail.com>
2019-05-02 06:45:34 +0600

    Fix OPTIONS cors

--- core/cors.php
+++ core/cors.php
@@ -17,5 +17,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
         header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    die();
 }
 
